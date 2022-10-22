4cafa1a6b3d323b1ccf760295840257dca68bb42
Kwu1010 <Kwu101020@gmail.com>
2022-10-21 23:16:19 -0400

    Google & Twitter Webscrapers

---
+++ google_webscraper.py
@@ -0,0 +1,154 @@
+"""
+Title: google_webscraper.py
+Developer: Kevin Wu
+Description: Webscraper for general Google searches.
+"""
+
+import requests
+from bs4 import BeautifulSoup
+from googlesearch import search
+import twitter_api_module
+
+"""
+Global variables (used as static)
+"""
+wiki = ["w", "wiki", "wikipedia"]
+google = ["g", "google"]
+
+"""
+Check if the specified website is valid.
+
+@param website is the website to verify
+@return a valid website
+"""
+def verify_website(website):
+    while(((website.lower() in wiki) == False) and (website.lower() in google) == False):
+        print("Invalid website.")
+        website = input("Search in [ Google | Wikipedia ]: ")
+    return website
+
+"""
+Check if search terms are valid.
+
+@param website is the website choice
+@param search_terms are the search terms to verify
+@return valid search terms
+"""
+def verify_search_terms(website, search_terms):
+    if(website in google):
+        while(search_terms.isspace()):
+            print("Invalid search. Search is empty.")
+            search_terms = input("Enter search term: ")
+        return search_terms
+    elif(website in wiki):
+        while (search_terms.isspace() or (len(search_terms.split(' ')) != 1)):
+            print("Invalid search. Search is empty or contains spaces.")
+            search_terms = input("Enter search term: ")
+        search_terms = search_terms.replace(' ', '')
+        return search_terms
+
+"""
+Query user for search terms and search website.
+
+@return search terms and the website to search in
+"""
+def specify_search_parameters():
+    website = input("Search in [ Google | Wikipedia ]: ")
+    website = verify_website(website)
+
+    search_terms = input("Enter search term: ")
+    search_terms = verify_search_terms(website, search_terms)
+
+    parse_search = -1
+    while(parse_search == -1):
+        parse_search = input("Filter search for sentence with keywords [ Y | N ]: ")
+        if(parse_search.lower() == "y"):
+            parse_search = 1
+        elif(parse_search.lower() == 'n'):
+            parse_search = 0
+    return search_terms, website, parse_search
+
+"""
+Filter the search to sentences which contain the search terms and print them out.
+
+@param search_terms are the terms to search for
+@param website_elements is the website data
+"""
+def filter_search(search_terms, website_elements):
+    term_list = search_terms.split(' ')
+    for data in website_elements.find_all("p"):
+        contains_term = False
+        text = data.get_text()
+        text = text.split(' ')
+        for word in text:
+            if word in term_list:
+                contains_term = True
+                break
+        if contains_term:
+            print(data.get_text())
+
+"""
+Examine if URL is a tweet.
+
+@param url is the URL to verify
+@return the tweet_id if the URL is a tweet, -1 otherwise
+"""
+def is_tweet(url):
+    tweet_id = twitter_api_module.validate_link(url)
+    return tweet_id
+
+"""
+Search google for the top 10 results of the search terms and print them out.
+
+@param search_terms are the terms to search
+@param parse_search is whether to filter the search 
+"""
+def search_google(search_terms, parse_search):
+    for url in search(search_terms, tld="co.in", num=10, stop=10, pause=2):
+        print(url)
+        tweet_id = is_tweet(url)
+        if(tweet_id == -1):
+            html = requests.get(url)
+            website_elements = BeautifulSoup(html.content, 'html.parser')
+            if(parse_search == 0):
+                for data in website_elements.find_all("p"):
+                    print(data.get_text())
+            else:
+                filter_search(search_terms, website_elements)
+        else:
+            print(twitter_api_module.return_text(url))
+
+"""
+Search the wiki for the search term.
+
+@param search_terms are the terms to search for
+@param parse_search is whether to filter the search
+"""
+def search_wiki(search_terms, parse_search):
+    url = "https://en.wikipedia.org/wiki/" + search_terms
+    html = requests.get(url)
+    website_elements = BeautifulSoup(html.content, 'html.parser')
+    if (parse_search == 0):
+        for data in website_elements.find_all("p"):
+            print(data.get_text())
+    else:
+        filter_search(search_terms, website_elements)
+
+"""
+Main function. Takes a bearer token and url to extract text from a tweet.
+"""
+def main():
+    search_terms, website, parse_search = specify_search_parameters()
+    if(website in google):
+        pass
+        search_google(search_terms, parse_search)
+    elif(website in wiki):
+        pass
+        search_wiki(search_terms, parse_search)
+
+"""
+Main function guard.
+"""
+if __name__ == "__main__":
+    main()
+
---
+++ twitter_api_module.py
@@ -0,0 +1,77 @@
+"""
+Title: twitter_api_module.py
+Developer: Kevin Wu
+Description: Webscraper module for Twitter URLs.
+"""
+
+import tweepy
+
+"""
+Login to tweepy using a Twitter bearer token.
+
+@param bearer_token is the user's bearer token
+@return the tweepy client
+"""
+def login(bearer_token):
+    client = tweepy.Client(bearer_token=bearer_token)
+    return client
+
+"""
+Validate that the given link is for a Twitter tweet.
+Looks for "twitter.com" and "status" in the specific positions in the link, then returns the ID of the tweet.
+Ends the program if the URL is invalid.
+
+@param url is the URL of the tweet
+@return the tweet ID if valid or -1 if invalid
+"""
+def validate_link(url):
+    url_split = url.split('/')
+    if (len(url_split) == 6 and (url_split[-4] == "twitter.com" and url_split[-2] == "status")):
+        tweet_id = url_split[-1]
+        return tweet_id
+    else:
+        return -1
+
+"""
+Return the text from a tweet.
+
+@param client is the tweepy client
+@param tweet_id is the id of the tweet to get
+@return the tweet
+"""
+def open_tweet_link(client, tweet_id):
+    tweet = client.get_tweet(id=tweet_id)
+    return tweet
+
+"""
+Return the text of a tweet given a url.
+
+@param url is the url of the tweet
+@return the text contained in the tweet
+"""
+def return_text(url):
+    bearer_token = "AAAAAAAAAAAAAAAAAAAAAPazZAEAAAAAqXtIWwkQ8%2BcWJVVa%2Fxe9El5X9YY%3DSViGWl8k0QIdhclQx4I5phPSyHz606eTkaBcLNlUCfpPzIsk5K"
+    client = login(bearer_token)
+    tweet_id = validate_link(url)
+    tweet = open_tweet_link(client, tweet_id)
+    return tweet.data.text
+
+"""
+Main function. Takes a bearer token and url to extract text from a tweet.
+"""
+def main():
+    bearer_token = "AAAAAAAAAAAAAAAAAAAAAPazZAEAAAAAqXtIWwkQ8%2BcWJVVa%2Fxe9El5X9YY%3DSViGWl8k0QIdhclQx4I5phPSyHz606eTkaBcLNlUCfpPzIsk5K"
+    url = "https://twitter.com/DefenceHQ/status/1578978756932571136"
+    client = login(bearer_token)
+    tweet_id = validate_link(url)
+    if tweet_id == -1:
+        print("URL is not a link to a tweet.")
+        exit(1)
+    tweet = open_tweet_link(client, tweet_id)
+    print(tweet.data.text)
+
+"""
+Main function guard.
+"""
+if __name__ == "__main__":
+    main()
