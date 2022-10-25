6c9358af767518d92763a0f4f799ef00a2a2c4bf
Kwu1010 <Kwu101020@gmail.com>
2022-10-25 12:35:39 -0400

    Interface
    +ico file
    ~reformatted layout

--- interface.py
+++ interface.py
@@ -1,37 +1,65 @@
-import turtle as tt
 import tkinter as tk
+from turtle import RawTurtle as rt
 
-def initialize_turtle():
-    tt.setup(800, 600)
-    tt.up()
-    tt.title("Webscraper")
-    tt.pensize(2)
-    tt.speed(2)
+def build_root(width, height):
+    root = tk.Tk()
+    root.title("Webscraper")
+    root.iconbitmap('papers.ico')
+    root.geometry(str(width) + "x" + str(height))
+    root.resizable(False, False)
+    return root
 
-def draw_background_design():
+def build_frames(root):
+    button_frame = tk.LabelFrame(root)
+    button_frame.pack(padx=2, pady=5)
+    button_frame.place(x=10, y=10)
+    return button_frame
+
+def build_canvas(root, width, height):
+    canvas = tk.Canvas(root, width=width, height=height)
+    canvas.pack()
+    return canvas
+
+def draw_background(canvas):
+    tt = rt(canvas)
+
     tt.begin_fill()
-    for x in range(4):
-        tt.forward(100)
-        tt.right(90)
+
+    tt.fillcolor("red")
+    tt.right(45)
+    tt.forward(100)
+    tt.right(45)
+    tt.forward(100)
+    tt.right(90)
+    tt.forward(100)
+    tt.right(45)
+    tt.forward(1000)
+    tt.right(45)
+    tt.forward(100)
+    tt.right(90)
+    tt.forward(100)
+    tt.right(45)
+    tt.forward(900)
     tt.end_fill()
 
-def place_buttons(canvas, screen):
-    button = tk.Button(canvas.master, text="Press me", command=press)
-    canvas.create_window(screen.window_width() / 2, screen.window_height() / 2, window=button)
-
-def press():
-    print("test")
+def place_buttons(button_frame):
+    input_field = tk.Entry(button_frame, width=131, text="Null")
+    input_field.pack()
+    return input_field
 
 def main():
-    initialize_turtle()
-    screen = tt.Screen()
-    screen.bgcolor("white")
-    draw_background_design()
+    width = 1200
+    height = 600
 
-    canvas = screen.getcanvas()
-    place_buttons(canvas, screen)
+    root = build_root(width, height)
 
-    tt.done()
+    canvas = build_canvas(root, width, height)
+    draw_background(canvas)
+
+    button_frame = build_frames(root)
+    place_buttons(button_frame)
+
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
