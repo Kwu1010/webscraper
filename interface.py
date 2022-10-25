0af94dc3049b9fc9198f67b59868623737fa5669
Kwu1010 <Kwu101020@gmail.com>
2022-10-25 00:38:23 -0400

    Interface
    function stubbing

--- interface.py
+++ interface.py
@@ -1,16 +1,36 @@
 import turtle as tt
 import tkinter as tk
 
+def initialize_turtle():
+    tt.setup(800, 600)
+    tt.up()
+    tt.title("Webscraper")
+    tt.pensize(2)
+    tt.speed(2)
+
+def draw_background_design():
+    tt.begin_fill()
+    for x in range(4):
+        tt.forward(100)
+        tt.right(90)
+    tt.end_fill()
+
+def place_buttons(canvas, screen):
+    button = tk.Button(canvas.master, text="Press me", command=press)
+    canvas.create_window(screen.window_width() / 2, screen.window_height() / 2, window=button)
+
 def press():
-    pass
+    print("test")
 
 def main():
+    initialize_turtle()
     screen = tt.Screen()
-    tt.forward(100)
     screen.bgcolor("white")
+    draw_background_design()
+
     canvas = screen.getcanvas()
-    button = tk.Button(canvas.master, text="Press me", command=press)
-    canvas.create_window(-200, -200, window=button)
+    place_buttons(canvas, screen)
+
     tt.done()
 
 if __name__ == "__main__":
